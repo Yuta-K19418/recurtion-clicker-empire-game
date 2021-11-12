bd9c3ff26d894b880a25665cc2ecf64603e2b769
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-11-12 10:13:42 +0000

    created user info

--- src/components/main.ts
+++ src/components/main.ts
@@ -6,12 +6,12 @@ const Main = () => {
   let mainContainer = document.getElementById("main");
 
   let subContainer = document.createElement("div");
-  subContainer.classList.add("d-flex", "col-11", "bg-navy", "d-block");
-  // subContainer.style.height = "100vh";
+  subContainer.classList.add("d-flex", "bg-navy", "d-block", "my-5", "mx-3");
+  subContainer.style.height = "100vh";
   subContainer.id = "mainContainer";
 
   let leftSection = document.createElement("div");
-  leftSection.classList.add("bg-dark","col-4", "my-2", "mx-1");
+  leftSection.classList.add("bg-dark","col-3", "my-2", "mx-1");
 
   let hamburgerSection = document.createElement("div");
   hamburgerSection.classList.add("bg-navy", "m-1");
@@ -51,6 +51,51 @@ const Main = () => {
   hamburgerImgDiv.append(hamburgerImg);
   leftSection.append(hamburgerImgDiv);
   subContainer.append(leftSection);
+
+
+  let rightSection = document.createElement("div");
+  rightSection.classList.add("col-9");
+  let userInfoDiv = document.createElement("div");
+  userInfoDiv.classList.add("d-flex","flex-wrap", "bg-dark", "my-2", "col-9");
+  let nameDiv = document.createElement("div");
+  nameDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  let nameP = document.createElement("p");
+  nameP.classList.add("text-white", "font-weight-bold");
+  nameP.innerHTML = "test";
+  nameDiv.append(nameP);
+  userInfoDiv.append(nameDiv);
+  
+  let ageDiv = document.createElement("div");
+  ageDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  let ageP = document.createElement("p");
+  ageP.classList.add("text-white", "font-weight-bold");
+  ageP.innerHTML = "1 years old";
+  ageDiv.append(ageP);
+  userInfoDiv.append(ageDiv);
+
+  let daysDiv = document.createElement("div");
+  daysDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  let daysP = document.createElement("p");
+  daysP.classList.add("text-white", "font-weight-bold");
+  daysP.innerHTML = "1 days";
+  daysDiv.append(daysP);
+  userInfoDiv.append(daysDiv);
+
+  let amountDiv = document.createElement("div");
+  amountDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  let amountP = document.createElement("p");
+  amountP.classList.add("text-white", "font-weight-bold");
+  amountP.innerHTML = "¥100";
+  amountDiv.append(amountP);
+  userInfoDiv.append(amountDiv);
+
+  rightSection.append(userInfoDiv);
+  
+  let upperAreaDiv = document.createElement("div");
+  let middleAreaDiv = document.createElement("div");
+  let lowerAreaDiv = document.createElement("div");
+
+  subContainer.append(rightSection);
 
   mainContainer?.append(subContainer);
 }
