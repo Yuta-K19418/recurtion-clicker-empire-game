449727460956507ee373bd21f272f442f176e818
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-11-23 07:41:32 +0000

    Updated hamburger box and user information box

--- src/components/main.ts
+++ src/components/main.ts
@@ -2,6 +2,7 @@ import AppConfig from "../appConfig";
 
 const Main = () => {
   const config = AppConfig();
+  const AMOUNT_PER_A_CLICK = 25;
 
   let mainContainer = document.getElementById("main");
 
@@ -11,7 +12,7 @@ const Main = () => {
   subContainer.id = "mainContainer";
 
   let leftSection = document.createElement("div");
-  leftSection.classList.add("bg-dark","col-3", "my-2", "mx-1");
+  leftSection.classList.add("bg-dark","col-5", "my-2", "mx-5");
 
   let hamburgerSection = document.createElement("div");
   hamburgerSection.classList.add("bg-navy", "m-1");
@@ -24,6 +25,7 @@ const Main = () => {
   numberOfhamburgersP.innerHTML = "0";
   upperHamburgerDiv.append(numberOfhamburgersP);
 
+  // Hamburger component in upper left section
   let BurgersP = document.createElement("div");
   BurgersP.classList.add("text-white", "font-weight-bold", "pl-1");
   BurgersP.innerHTML = "Burgers";
@@ -35,28 +37,36 @@ const Main = () => {
 
   let oneClickP = document.createElement("p");
   oneClickP.classList.add("text-white", "font-weight-bold");
-  oneClickP.innerHTML = "one click ￥25";
+  oneClickP.innerHTML = `one click ￥${AMOUNT_PER_A_CLICK}`;
   lowerHamburgerDiv.append(oneClickP);
   hamburgerSection.append(lowerHamburgerDiv);
 
   leftSection.append(hamburgerSection);
 
   let hamburgerImgDiv = document.createElement("div");
-  hamburgerImgDiv.classList.add("d-flex", "align-items-start", "justify-content-center", "py-5");
+  hamburgerImgDiv.classList.add("d-flex", "align-items-start", "justify-content-center", "py-5", "hover");
   
+  // Hamburger image in lower left section
   let hamburgerImg = document.createElement("img");
   hamburgerImg.src = config.hamburgerImg;
-  hamburgerImg.height = 300;
-  hamburgerImg.width = 400;
+  hamburgerImg.height = 200;
+  hamburgerImg.width = 300;
+  hamburgerImg.addEventListener("click", function(){
+    numberOfhamburgersP.innerHTML = (parseInt(numberOfhamburgersP.innerHTML) + 1).toString();
+    numberOfAmountP.innerHTML = (parseInt(numberOfAmountP.innerHTML) + AMOUNT_PER_A_CLICK).toString();
+  });
   hamburgerImgDiv.append(hamburgerImg);
   leftSection.append(hamburgerImgDiv);
   subContainer.append(leftSection);
 
 
   let rightSection = document.createElement("div");
-  rightSection.classList.add("col-9");
+  //rightSection.classList.add("col-10");
+
+  // User information
+  // 1.User name
   let userInfoDiv = document.createElement("div");
-  userInfoDiv.classList.add("d-flex","flex-wrap", "bg-dark", "my-2", "col-9");
+  userInfoDiv.classList.add("d-flex","flex-wrap", "bg-dark", "my-2");
   let nameDiv = document.createElement("div");
   nameDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
   let nameP = document.createElement("p");
@@ -65,28 +75,50 @@ const Main = () => {
   nameDiv.append(nameP);
   userInfoDiv.append(nameDiv);
   
+  // 2.User age
   let ageDiv = document.createElement("div");
-  ageDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  ageDiv.classList.add("d-flex", "flex-row", "justify-content-center", "bg-navy", "col-5", "m-1", "p-2");
+  let numberOfAgeP = document.createElement("p");
+  numberOfAgeP.classList.add("text-white", "font-weight-bold","pr-1");
+  numberOfAgeP.innerHTML = "1";
+  ageDiv.append(numberOfAgeP);
   let ageP = document.createElement("p");
   ageP.classList.add("text-white", "font-weight-bold");
-  ageP.innerHTML = "1 years old";
+  ageP.innerHTML = "years old";
   ageDiv.append(ageP);
   userInfoDiv.append(ageDiv);
 
+  // 3.Passed days
   let daysDiv = document.createElement("div");
-  daysDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
+  daysDiv.classList.add("d-flex", "flex-row", "justify-content-center", "bg-navy", "col-5", "m-1", "p-2");
+  let numberOfDaysP = document.createElement("p");
+  numberOfDaysP.classList.add("text-white", "font-weight-bold","pr-1");
+  numberOfDaysP.innerHTML = "1";
+  setInterval(function(){
+    numberOfDaysP.innerHTML = (parseInt(numberOfDaysP.innerHTML) + 1).toString();
+    if (parseInt(numberOfDaysP.innerHTML) % 365 === 0)
+    {
+      numberOfAgeP.innerHTML = (parseInt(numberOfAgeP.innerHTML) + 1).toString();
+    }
+  }, 1000);
+  daysDiv.append(numberOfDaysP);
   let daysP = document.createElement("p");
   daysP.classList.add("text-white", "font-weight-bold");
-  daysP.innerHTML = "1 days";
+  daysP.innerHTML = "days";
   daysDiv.append(daysP);
   userInfoDiv.append(daysDiv);
 
+  // 4.Amount
   let amountDiv = document.createElement("div");
-  amountDiv.classList.add("text-center", "bg-navy", "col-5", "m-1", "p-2");
-  let amountP = document.createElement("p");
-  amountP.classList.add("text-white", "font-weight-bold");
-  amountP.innerHTML = "¥100";
-  amountDiv.append(amountP);
+  amountDiv .classList.add("d-flex", "flex-row", "justify-content-center", "bg-navy", "col-5", "m-1", "p-2");
+  let currencyP = document.createElement("p");
+  currencyP .classList.add("text-white", "font-weight-bold",);
+  currencyP .innerHTML = "¥";
+  amountDiv.append(currencyP);
+  let numberOfAmountP = document.createElement("p");
+  numberOfAmountP .classList.add("text-white", "font-weight-bold");
+  numberOfAmountP.innerHTML = "0";
+  amountDiv.append(numberOfAmountP);
   userInfoDiv.append(amountDiv);
 
   rightSection.append(userInfoDiv);
